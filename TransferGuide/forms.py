7dde9c6565e01d50559f9268ae55c47edd76e8fd
ec5ug <ec5ug@virginia.edu>
2023-03-17 15:55:40 -0400

    reformatted form

--- TransferGuide/forms.py
+++ TransferGuide/forms.py
@@ -4,9 +4,10 @@ from django import forms
  
 # creating a form
 class addCourseForm(forms.Form):
+    course_institution = forms.CharField(max_length=100)
     course_name = forms.CharField(max_length = 100)
+    course_dept = forms.CharField(max_length=100)
     course_number = forms.CharField(max_length = 100)
-    course_url = forms.CharField(max_length = 200)
-    course_description = forms.CharField(max_length = 500)
-    course_institution = forms.CharField(max_length = 100)
+    course_grade = forms.CharField(max_length=1,widget=forms.Select(choices=[('a','A'),('b','B'),('c','C'),('d','D'),('f','F')]))
+
 
