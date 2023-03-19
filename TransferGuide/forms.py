53f4ea2ee9ec47fd0c012072ef2f9e1e2ff5d0b0
SaidHoja <saidhojabusiness@gmail.com>
2023-03-18 20:46:23 -0400

    somewhat working sis search

--- TransferGuide/forms.py
+++ TransferGuide/forms.py
@@ -10,5 +10,11 @@ class addCourseForm(forms.Form):
     course_number = forms.CharField(max_length=10)
     course_grade = forms.CharField(max_length=1,widget=forms.Select(choices=[('A','A'),('B','B'),('C','C'),('D','D'),
                                                                              ('F','F')]))
+class sisForm(forms.Form):
+    subject = forms.CharField(max_length=10,required = False)
+    term = forms.CharField(max_length=6,widget=forms.Select(choices=[(8, "FALL"), (3, "SPRING")]))
+    year = forms.IntegerField(max_value=99)
+    instructor = forms.CharField(max_length=20, required = False)
+
 
 
