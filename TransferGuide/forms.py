a4a2a4375f2e9132d3131f74490347e8a0248eab
sususuint <113937210+sususuint@users.noreply.github.com>
2023-03-19 06:19:56 -0400

    updated website buttons and headers

--- TransferGuide/forms.py
+++ TransferGuide/forms.py
@@ -12,7 +12,7 @@ class addCourseForm(forms.Form):
     course_grade = forms.CharField(max_length=1,widget=forms.Select(choices=[('A','A'),('B','B'),('C','C'),('D','D'),
                                                                              ('F','F')]))
 class sisForm(forms.Form):
-    subject = forms.CharField(label='Subject (e.g. CS, ASTR, etc.)', max_length=10, required = False)
+    subject = forms.CharField(label='Subject (e.g. CS, ASTR, etc.)', max_length=5, required = False)
     term = forms.CharField(max_length=6,widget=forms.Select(choices=[(8, "FALL"), (3, "SPRING")]))
 
     YEAR_CHOICES = []
