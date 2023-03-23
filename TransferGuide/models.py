dc4c359c87c2f35f3c83772516dc182beb14fd5c
ec5ug <ec5ug@virginia.edu>
2023-03-23 00:05:26 -0400

    created model Viable_Course for next feature

--- TransferGuide/models.py
+++ TransferGuide/models.py
@@ -18,6 +18,15 @@ class Course(models.Model):
     def __str__(self):
         return str(self.username) + " " + str(self.course_name)
 
+class Viable_Course(models.Model):
+    username = models.ForeignKey(User,on_delete = models.CASCADE)
+    course_institution = models.CharField(max_length=100, null=True, blank=True)
+    course_name = models.CharField(max_length=100, null=True, blank=True)
+    course_dept_num = models.CharField(max_length=100, null=True, blank=True)
+    course_grade = models.CharField(max_length=1, null=True, blank=True)
+    def __str__(self):
+        return str(self.username) + " " + str(self.course_name)
+
 class Comment(models.Model):
     title = models.CharField(max_length=200)
     comment_text =models.CharField(max_length=200)
