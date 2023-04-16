979d93691330d56f12bfbe2506b1338d0b96ee10
ec5ug <ec5ug@virginia.edu>
2023-04-15 23:51:47 -0400

    added tests for searchCourseForm

--- TransferGuide/tests.py
+++ TransferGuide/tests.py
@@ -2,7 +2,7 @@ import datetime
 from django.test import TestCase
 from django.urls import reverse
 from .models import Course
-from .forms import requestCourseForm
+from .forms import requestCourseForm, searchCourseForm
 
 class FormViewTests(TestCase):
     def test_form_load(self):
@@ -59,6 +59,51 @@ class LoginViewTests(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+class Search_Course_Form(TestCase):
+    def test_blank_form(self):
+        form_data = {"institution":"", "word":"", "dept_num":""}
+        form = searchCourseForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    def test_equivalence_case(self):
+        form_data = {"institution":"Piedmont Virginia Community College", "word":"eq", "dept_num":"ENGR 1000"}
+        form = searchCourseForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    def test_invalid_dept_has_num(self):
+        form_data = {"institution":"", "word":"", "dept_num":"4 1000"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_invalid_dept_invalid_char(self):
+        form_data = {"institution":"", "word":"", "dept_num":"!! 1000"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_valid_dept_hetero_cases(self):
+        form_data = {"institution":"", "word":"", "dept_num":"Engr 1000"}
+        form = searchCourseForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    def test_invalid_num_negative(self):
+        form_data = {"institution": "", "word": "", "dept_num": "ENGR -1000"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_invalid_num_zero(self):
+        form_data = {"institution": "", "word": "", "dept_num": "ENGR 0"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_invalid_num_too_small(self):
+        form_data = {"institution": "", "word": "", "dept_num": "ENGR 1"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_invalid_num_uses_letters(self):
+        form_data = {"institution": "", "word": "", "dept_num": "ENGR IS"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_invalid_num_exclamation(self):
+        form_data = {"institution": "", "word": "", "dept_num": "ENGR !"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    def test_invalid_too_many_words(self):
+        form_data = {"institution": "", "word": "", "dept_num": "ENGR 1000 1"}
+        form = searchCourseForm(data=form_data)
+        self.assertFalse(form.is_valid())
 # class CourseListTests(TestCase):
 #     def test_course_list_load(self):
 #         response = self.client.get(reverse('requestCourseList'))
