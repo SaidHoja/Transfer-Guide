ce09c2605f59e4e1c27be17ef8a01c2195c7acf3
ec5ug <ec5ug@virginia.edu>
2023-04-16 04:04:24 -0400

    commented out tests

--- TransferGuide/models.py
+++ TransferGuide/models.py
@@ -10,7 +10,7 @@ class Course(models.Model):
     course_institution = models.CharField(max_length=100, null=False, blank=True)
     course_name = models.CharField(max_length=100, null=False, blank=True)
     course_dept = models.CharField(max_length=100, null = True)
-    course_num = models.PositiveIntegerField(null= True, validators=[MinValueValidator(1)]);
+    course_num = models.PositiveIntegerField(null= True, validators=[MinValueValidator(1)])
     course_grade = models.CharField(max_length=1, null=False, blank=True)
     course_delivery = models.CharField(max_length=10, null=False, blank=True)
     syllabus_url = models.URLField(null=True, blank=True)
