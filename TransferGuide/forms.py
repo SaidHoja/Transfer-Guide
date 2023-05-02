104483c3d0f45d2ccdecb4223012e84ef56cd5a5
ec5ug <ec5ug@virginia.edu>
2023-05-01 22:28:04 -0400

    UVA-taught course submitted as a transfer request has error pop up

--- TransferGuide/forms.py
+++ TransferGuide/forms.py
@@ -27,8 +27,7 @@ def validate_is_a_word(value):
             raise ValidationError("Please enter a valid word. Uses characters A-Z")
 
 class requestCourseForm(forms.Form):
-    course_institution = forms.CharField(max_length=100, validators=[validate_non_uva],
-                                         widget=forms.TextInput(attrs={'placeholder': 'Enter the instutition your course is from.'}))
+    course_institution = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter the instutition your course is from.'}))
     course_name = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'placeholder': 'Enter the name of your course'}))
     course_dept = forms.CharField(label="Course department",max_length=5, validators=[validate_one_word], widget=forms.TextInput(attrs={'placeholder': 'Example: enter MATH for transfer course MATH 1000'}))
     course_number = forms.IntegerField(min_value=0, max_value=9999)
